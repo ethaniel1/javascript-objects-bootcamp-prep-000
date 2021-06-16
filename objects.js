95bbef59f0300e60c242dfb9d5ba6a834150f015
Jonathan  Zylberberg <zylberberg.jonathan@gmail.com>
2021-06-16 04:10:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = {
   jimmyBuffet: 'Margaritaville'
-  
-};
+  johnMayer: 'Daughters'
+ 
+}
 
 function updatePlaylist (playlist, artistName, songTitle) {
 (playlist, ['johnMayer'], 'Daughters')}
