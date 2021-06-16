09ba4c08be0dc1ffba9a9d4f2f15b6707e656ace
Jonathan  Zylberberg <zylberberg.jonathan@gmail.com>
2021-06-16 04:19:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,7 +6,10 @@ function updatePlaylist (playlist, artist, song) {
 playlist[artist] = song 
 return playlist }
 
-function removeFromPlaylist
+function removeFromPlaylist (playlist, artist) {
+delete playlist.artist
+return playlist 
+}
 
 
 
